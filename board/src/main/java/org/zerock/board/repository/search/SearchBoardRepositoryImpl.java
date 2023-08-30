9fd23b7fd0ad3099a051415e3133e6aca4898310
SHIN-HANBEEN <shb0801@naver.com>
2023-08-30 20:49:56 +0900

    298 search1( ) Error 발생중..

---
+++ board/src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.zerock.board.repository.search;
+
+import com.querydsl.jpa.JPQLQuery;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+
+import java.util.List;
+
+@Log4j2
+public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository{
+    public SearchBoardRepositoryImpl() {
+        super(Board.class);
+    }
+
+    @Override
+    public Board search1() {
+        log.info("search1............");
+
+        QBoard board = QBoard.board; //Q 도메인 클래스 준비
+        JPQLQuery<Board> jpqlQuery = from(board); //JPQLQuery 준비(Querydsl 은 JPQLQuery 인터페이스를 갖고 있다)
+        jpqlQuery.select(board).where(board.bno.eq(1L)); //SQL문 생성
+
+        log.info("-------------");
+        log.info(jpqlQuery);
+        log.info("-------------");
+
+        List<Board> result = jpqlQuery.fetch();
+
+        return null;
+    }
+}
