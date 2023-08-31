3bacabd02a09cb918b76d72a63554f5aeccff07a
SHIN-HANBEEN <shb0801@naver.com>
2023-08-31 09:31:19 +0900

    build.gradle 설정 오류 해결

--- board/src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ board/src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,10 +1,13 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import lombok.extern.log4j.Log4j2;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.QBoard;
+
+
 
 import java.util.List;
 
@@ -18,8 +21,8 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     public Board search1() {
         log.info("search1............");
 
-        QBoard board = QBoard.board; //Q 도메인 클래스 준비
-        JPQLQuery<Board> jpqlQuery = from(board); //JPQLQuery 준비(Querydsl 은 JPQLQuery 인터페이스를 갖고 있다)
+        QBoard board = QBoard.board;//Q 도메인 클래스 준비
+        JPQLQuery<Board> jpqlQuery = from(board);//JPQLQuery 준비(Querydsl 은 JPQLQuery 인터페이스를 갖고 있다)
         jpqlQuery.select(board).where(board.bno.eq(1L)); //SQL문 생성
 
         log.info("-------------");
